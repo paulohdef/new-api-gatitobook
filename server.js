3d46472057766a33933e6efb8d2ae033f04ce156
Paulo de Freitas <paulohdef1@gmail.com>
2021-11-12 09:25:49 -0300

    modificando environment heroku

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http')
     ,app = require('./config/express');
     
-http.createServer(app).listen(3000, function() {
-    console.log('Servidor escutando na porta: ' + this.address().port);
+http.createServer(app).listen(process.env.PORT || 5000 , '0.0.0.0', function(){
+
+console.log("Server started...");
 });
+
+//listen(3000, function() {
+ //  console.log('Servidor escutando na porta: ' + this.address().port);
+//}    --> porta utilizada para o ambiente de desenvolvimento
+
+
 
